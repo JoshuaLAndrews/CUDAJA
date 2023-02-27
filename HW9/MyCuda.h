5db9c8cdd3e6759907e953f5888f6ea300b17d0c
JoshuaLAndrews <122764829+JoshuaLAndrews@users.noreply.github.com>
2023-02-27 14:10:43 -0600

    Add files via upload

---
+++ HW9/MyCuda.h
@@ -0,0 +1,12 @@
+void myCudaErrorCheck(const char *file, int line)
+{
+cudaError_t error;
+error = cudaGetLastError();
+
+	if(error != cudaSuccess)
+	{
+		printf("\n CUDA message = %s, File = %s, Line = %d\n", cudaGetErrorString(error), file, line);
+		exit(0);
+	}
+}
+
